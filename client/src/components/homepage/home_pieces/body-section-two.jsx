52acb8219a0f67ab249ccb3b9ab97d7960e48f5a
Christian Anderson <tehninjawaffle@gmail.com>
2024-07-07 09:35:56 -0700

    Implemented section 1, only work needing to be done is with the button

--- client/src/components/homepage/home_pieces/body-section-two.jsx
+++ client/src/components/homepage/home_pieces/body-section-two.jsx
@@ -6,11 +6,21 @@
 
 const BodySectionTwo = () => {
 
+  const img = require("../../../media/chefSectionTwo.jpg")
 
   return (
     <>
-      <section>
-        Two
+      <section id="sectionTwo">
+        <div id="sectionTwoWrapper">
+          <p className="sectionContent">
+            One of our proudest implementations is a twitter like feed from <em className="sectionTwoTextChange">Real chefs</em> around the world!
+          </p>
+          <p className="sectionContent">
+            Sort by food type, region, even specific chef to find what you desire. Click the link Below to get started on finding the recipe <em className="sectionTwoTextChange">just right for you</em>!
+          </p>
+          <input type="button" value="Click me!"/>
+        </div>
+        <img className="homeImage" src={img} alt="" />
       </section>
     </>
   )
