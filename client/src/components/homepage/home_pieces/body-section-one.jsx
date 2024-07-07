17235f12c501943c88789f32a98db63d2a1c5a8c
Christian Anderson <tehninjawaffle@gmail.com>
2024-07-07 09:18:37 -0700

    'Finished' main one, setup and styling in place

--- client/src/components/homepage/home_pieces/body-section-one.jsx
+++ client/src/components/homepage/home_pieces/body-section-one.jsx
@@ -1,17 +1,17 @@
 
 
-
-
-
-
 const BodySectionOne = () => {
-
+  const image = require("../../../media/foodSpreadMainOne.jpg")
 
   return (
     <>
-      <section>
-        One
-      </section>
+      <main id="mainOne">
+        <div id="mainWrapper">
+          <p className="mainContent">Ever feel like the cookbooks <em className="mainTextChange">aren't enough</em>? Has the line cook given you one too many dish ideas that <em className="mainTextChange">went up in flames</em>? Worry not!</p>
+          <p className="mainContent" style={{fontFamily: "cursive"}}>chef<span className="mainTextChange">cipe</span> is a platform for chefs like yourself to connect and share recipes! Our strong community <em className="mainTextChange">cooks their hearts out</em> to share their ideas & recipes with people like yourselves</p>
+        </div>
+        <img className="homeImage" src={image} alt="A spread of food of various types"/>
+      </main>
     </>
   )
 }
