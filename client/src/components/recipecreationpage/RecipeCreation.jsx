866a956573d75a6f91724a16bcd09f2e90703763
Christian Anderson <tehninjawaffle@gmail.com>
2024-07-08 07:33:50 -0700

    Created input name and input items, as well as a whole bunch of CSS

--- client/src/components/recipecreationpage/RecipeCreation.jsx
+++ client/src/components/recipecreationpage/RecipeCreation.jsx
@@ -1,19 +1,48 @@
 
 import React, { useState } from "react"
 
+import "../../styles/Recipe.scss"
 
 
 const RecipeCreationPage = () => {
 
   const [recipe, setRecipe] = useState()
-  const [ingredients, setIngredients] = useState()
+  const [ingredients, setIngredients] = useState([
+    { name: "", desc: "", quant: "", quantNum: "" }
+  ])
   const [steps, setSteps] = useState()
 
   return (
-    <main>
+    <main id="recipeCreationMainWrapper">
       <h1>Create your recipe!</h1>
-      <form>
-        <div></div>
+      <form id="recipeCreationForm">
+        <div id="dishNameWrapper">
+          <input type="text" value="" placeholder="Dish name..." />
+          <input type="button" value="Clear" />
+        </div>
+        <div className="divider"></div>
+        <div id="dishItemsWrapper">
+          <div id="dishItemsSubWrapper">
+            <p>Give your dish items!</p>
+            <input type="button" />
+          </div>
+          {
+            ingredients.map((val, key) => {
+              console.log("this is the value: ", val)
+              console.log("this is the key: ", key)
+              return (
+                <div className="itemsWrapper">
+                  <input type="text" value={val.name} placeholder="Item name..." />
+                  <input type="text" value={val.desc} placeholder="Item desc...(optional)" />
+                  <div className="itemQuantityWrapper">
+                    <input type="text" value={val.quant} placeholder="Item quant..." />
+                    <input type="number" value={val.quantNum} placeholder="Amount..." />
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
       </form>
     </main>
   )
