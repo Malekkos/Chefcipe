ffd6828f8605164dd1faeff1a26515bca1cb557f
Christian Anderson <tehninjawaffle@gmail.com>
2024-07-08 04:42:38 -0700

    Cleaned up code

--- client/src/components/recipepage/recipe_pieces/recipepagetop.jsx
+++ client/src/components/recipepage/recipe_pieces/recipepagetop.jsx
@@ -18,45 +18,28 @@ const RecipePageTop = () => {
   
   
   const changeActive = (event) => {
-    // console.log(event.target.id)
+    
     if (event.target.className === "recipePageSorterButton active") {
       return
     } else {
-      // console.log("this is the sortButtonActive: ", sortButtonActive, " this is the dummy one: ", initialSortButton)
       const changer = [...initialSortButton]
       changer[event.target.id].active = true
-      // console.log(changer)
-      // console.log("before", sortButtonActive)
       setSortButtonActive(changer)
-      // console.log("during", sortButtonActive)
-      // setSortButtonActive({
-      //   ...sortButtonActive,
-      //    [changer]: true
-      //   })
-      // console.log("after", sortButtonActive)
     }
-    // console.log(targ)
-
   }
-  console.log(sortButtonActive)
+
   return(
     <>
       <div id="recipePageTopWrapper">
         <div id="recipePageSorterWrapper">
           <p>Sort by:</p>
           { sortButtonActive.map((val, key) => {
-            // console.log("this is the val: ", val)
-            // console.log(val.active)
-            // console.log("this is the key:", key)
             return (
               <>
                 <input id={key} onClick={(event) => changeActive(event)} className={`recipePageSorterButton ${val.active ? "active": ""}`} type="button" value={`${val.name}`} />
               </>
             )
           })}
-          {/* <input onClick={(event) => changeActive(event)} className="recipePageSorterButton active" type="button" value="recent"/>
-          <input onClick={(event) => changeActive(event)} className="recipePageSorterButton" type="button" value="nationality" />
-          <input onClick={(event) => changeActive(event)} className="recipePageSorterButton" type="button" value="release" /> */}
         </div>
         <input type="button" value="Create Recipe" />
       </div>
