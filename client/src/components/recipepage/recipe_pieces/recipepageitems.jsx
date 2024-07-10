593ba22dde42949de6256b999069583b2964ee83
Christian Anderson <tehninjawaffle@gmail.com>
2024-07-10 10:37:57 -0700

    I dont want to talk about it

--- client/src/components/recipepage/recipe_pieces/recipepageitems.jsx
+++ client/src/components/recipepage/recipe_pieces/recipepageitems.jsx
@@ -34,33 +34,69 @@ const RecipePageItems = () => {
                     )
                   } else if (keys === "ingredients") {
                     let ingredients = val[keys]
+                    let changeElement = "<p className='itemInner'>"
                     for (let i = 0; i < ingredients.length; i++) {
-
                       if (ingredients[i] === ":") {
                         ingredients = ingredients.split("")
                         ingredients.splice(i, 1, "<br />")
                         ingredients = ingredients.join("")
                       }
-
-                      if (ingredients[i] === "I" && ingredients[i + 9] === "t") {
+                      
+                      if ( i !== 0 && ingredients[i] === "I" && ingredients[i + 9] ==="t") {
+                        console.log("found")
+                        console.log(i)
                         ingredients = ingredients.split("")
-                        ingredients.splice(i, 0, "<br />")
+                        ingredients.splice(i, 0, "<p />")
                         ingredients = ingredients.join("")
                         i++
                         i++
                         i++
                         i++
                         i++
-                        i++
+                      }
+
+                      if (ingredients[i] === "I" && ingredients[i + 12] === "e") {
+                        ingredients = ingredients.split("")
+                        ingredients.splice(ingredients[i], 0, changeElement)
+                        ingredients = ingredients.join("")
+                        for(let length = 0; length < 27; length++) {
+                          i++
+                        }
+                      }
+
+                      if (ingredients[i] === "I" && ingredients[i + 21] === "n") {
+                        ingredients = ingredients.split("")
+                        ingredients.splice(i, 0, changeElement)
+                        ingredients = ingredients.join("")
+                        for(let length = 0; length < 31; length++) {
+                          i++
+                        }
+                      }
+
+                      if (ingredients[i] === "I" && ingredients[i + 32] === "e") {
+                        ingredients = ingredients.split("")                        
+                        ingredients.splice(i, 0, changeElement)
+                        ingredients = ingredients.join("")
+                        for(let length = 0; length < 31; length++) {
+                          i++
+                        }
+                      }
+
+                      if(ingredients[i] === "I" && ingredients[i + 37] === "t") {
+                        ingredients = ingredients.split("")
+                        ingredients.splice(i, 0, changeElement)
+                        ingredients = ingredients.join("")
+                        for(let length = 0; length < 31; length++) {
+                          i++
+                        }
                       }
                     }
-                    
-                    let newIngredients = new DOMParser().parseFromString(`<div className="itemsIngredientsWrapper">${ingredients}</div>`, "text/html")
+
+                    let newIngredients = new DOMParser().parseFromString(`<div className="itemsIngredientsWrapr">${ingredients}</div>`, "text/html")
                     let arrayIngredients = (newIngredients.firstChild.lastChild.firstChild.innerHTML)
                     return (
-                      <>
-                        <div className="itemsIngredientsWrapper" dangerouslySetInnerHTML={{ __html: arrayIngredients }}></div>
-                      </>
+                        <div className="itemsIngredientsWrapper" dangerouslySetInnerHTML={{ __html: `<p>${arrayIngredients}</p>` }}>
+                        </div>
                     )
                   } else {
 
